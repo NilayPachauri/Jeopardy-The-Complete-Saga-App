e8f70e9174eaa95fa037d7a08be3819512ceaf08
nilaypachauri <pachauri@usc.edu>
2020-11-17 14:23:09 -0800

    Added QuestionPageViewController and Question Page View

---
+++ Jeopardy! The Complete Saga/Jeopardy! The Complete Saga/Controller/QuestionPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuestionPageViewController.swift
+//  Jeopardy! The Complete Saga
+//
+//  Created by Nilay Pachauri on 11/17/20.
+//
+
+import UIKit
+
+class QuestionPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
