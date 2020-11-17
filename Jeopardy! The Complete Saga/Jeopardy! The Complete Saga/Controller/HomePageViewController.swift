b7978ddcdf6c457c075134eb0760232c1d41c141
nilaypachauri <pachauri@usc.edu>
2020-11-17 13:06:17 -0800

    Added a Tap Gesture Recognizer to the Trivia Gauntlet Labels

--- Jeopardy! The Complete Saga/Jeopardy! The Complete Saga/Controller/HomePageViewController.swift
+++ Jeopardy! The Complete Saga/Jeopardy! The Complete Saga/Controller/HomePageViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class HomePageViewController: UIViewController {
 
+    @IBOutlet weak var triviaGauntletLabel: UILabel!
+    @IBOutlet weak var customGameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Add Tap Gesture Recognizer to Trivia Gauntlet
+        let triviaGauntletLabelTap = UITapGestureRecognizer(target: self, action: #selector(self.segueToTriviaGauntletView(_:)))
+        self.triviaGauntletLabel.isUserInteractionEnabled = true
+        self.triviaGauntletLabel.addGestureRecognizer(triviaGauntletLabelTap)
+        
+        // Add Tap Gesture Recognizer to Custom Game
+        let customGameLabelTap = UITapGestureRecognizer(target: self, action: #selector(self.segueToCustomGameView(_:)))
+        self.customGameLabel.isUserInteractionEnabled = true
+        self.customGameLabel.addGestureRecognizer(customGameLabelTap)
+    }
+    
+    // MARK: Functions to Segue to Other Views
+    
+    @objc
+    func segueToTriviaGauntletView(_ sender: UITapGestureRecognizer) {
+        // TODO: Segue into Trivia Gauntlet View
+        print("Label tapped")
+    }
+    
+    @objc
+    func segueToCustomGameView(_ sender: UITapGestureRecognizer) {
+        // TODO: Segue into Custom Game View
+        print("Label tapped")
     }
 
+    @IBAction func settingsButtonPressed(_ sender: UIButton) {
+        // TODO: Segue into Settings View
+    }
+    
+    @IBAction func infoButtonPressed(_ sender: UIButton) {
+        // TODO: Segue into Info View
+    }
 
 }
 
