da5e7b3ed79af9088848a3d7c28ed40e4b89176c
nilaypachauri <pachauri@usc.edu>
2020-11-17 21:16:14 -0800

    Updated Text Field when Slider Change

--- Jeopardy! The Complete Saga/Jeopardy! The Complete Saga/Controller/TriviaGauntletStartPageViewController.swift
+++ Jeopardy! The Complete Saga/Jeopardy! The Complete Saga/Controller/TriviaGauntletStartPageViewController.swift
@@ -9,10 +9,19 @@ import UIKit
 
 class TriviaGauntletStartPageViewController: UIViewController {
 
-    @IBOutlet weak var triviaGauntletLabel: UILabel!
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var optionsView: UIView!
     @IBOutlet weak var startLabel: UILabel!
+    
+    @IBOutlet weak var numberOfQuestionsTextField: UITextField!
+    @IBOutlet weak var numberOfQuestionsSlider: UISlider!
+    @IBOutlet weak var jeopardyQuestionsSwitch: UISwitch!
+    @IBOutlet weak var doubleJeopardyQuestionsSwitch: UISwitch!
+    @IBOutlet weak var finalJeopardySwitch: UISwitch!
+    @IBOutlet weak var difficulty1QuestionsSwitch: UISwitch!
+    @IBOutlet weak var difficulty2QuestionsSwitch: UISwitch!
+    @IBOutlet weak var difficulty3QuestionsSwitch: UISwitch!
+    @IBOutlet weak var difficulty4QuestionsSwitch: UISwitch!
+    @IBOutlet weak var difficulty5QuestionsSwitch: UISwitch!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,11 +30,29 @@ class TriviaGauntletStartPageViewController: UIViewController {
         setupBorders()
     }
     
-    // MARK: - Function to Set Up View
+    // MARK: - Functions to Set Up View
     func setupBorders() {
         self.startLabel.layer.borderWidth = 3
         self.startLabel.layer.borderColor = UIColor.link.cgColor
         self.startLabel.layer.cornerRadius = 15
+    }
+    
+    // MARK: - Functions to Control View
+    func getIntegerValueFromSlider(slider: UISlider) -> Int {
+        var value = slider.value
+        value.round()
+        
+        return Int(value)
+    }
+    
+    
+    @IBAction func numberOfQuestionsSliderValueChanged(_ sender: UISlider) {
+        
+        // Get the new value from the Slider
+        let value = self.getIntegerValueFromSlider(slider: self.numberOfQuestionsSlider)
+        
+        // Update the Text Field
+        self.numberOfQuestionsTextField.text = String(format: "%d", value)
     }
     
 
