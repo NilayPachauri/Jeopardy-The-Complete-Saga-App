71f7fcfa82fe8e2dd776c110c30d507d93c5fa37
nilaypachauri <pachauri@usc.edu>
2020-11-17 16:46:26 -0800

    Updated Question View to also include the Utility Function to Change Text Field Font

--- Jeopardy! The Complete Saga/Jeopardy! The Complete Saga/Controller/QuestionPageViewController.swift
+++ Jeopardy! The Complete Saga/Jeopardy! The Complete Saga/Controller/QuestionPageViewController.swift
@@ -9,10 +9,50 @@ import UIKit
 
 class QuestionPageViewController: UIViewController {
 
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerTextField: UITextField!
+    
+    let timerInterval: TimeInterval = 0.1
+    var timerLeft: Double = 0
+    var timer: Timer? = nil
+    var score: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.setAnswerTextFieldFont()
+        
+        // Initialize Values
+        self.timerLeft = 10.0
+        self.timer = Timer.scheduledTimer(timeInterval: self.timerInterval, target: self, selector: #selector(self.timerAction), userInfo: nil, repeats: true)
+    }
+    
+    // MARK: Functions to Set Up View
+    func setAnswerTextFieldFont() {
+        
+        // Get the Font from the Score Label
+        let font = UIFont(name: self.answerTextField.font?.fontName ?? "Swiss  911", size: Utility.getApproximateAdjustedFontSizeWithLabel(label: self.scoreLabel))
+        
+        // Set the Answer Text Field Font
+        self.answerTextField.font = font
+    }
+    
+    // MARK: Functions to Set Up Timer
+    @objc
+    func timerAction() {
+        // Decrement Time Left
+        self.timerLeft -= self.timerInterval
+        
+        // Update The Timer Label
+        self.timerLabel.text = String(format: "Timer: %3.1f", self.timerLeft)
+        
+        // Check if time expired
+        if self.timerLeft <= 0.01, let timer = self.timer {
+            timer.invalidate()
+        }
     }
     
 
