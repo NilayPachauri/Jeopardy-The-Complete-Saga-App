25bc4ac2ef5034d95a1afe925bdf8645e775b768
nilaypachauri <pachauri@usc.edu>
2020-11-18 13:53:46 -0800

    Updated FirestoreWrapper to try and get the counters

--- Jeopardy! The Complete Saga/Jeopardy! The Complete Saga/Model/FirestoreWrapper.swift
+++ Jeopardy! The Complete Saga/Jeopardy! The Complete Saga/Model/FirestoreWrapper.swift
@@ -1,5 +1,5 @@
 //
-//  ModelUtility.swift
+//  FirestoreWrapper.swift
 //  Jeopardy! The Complete Saga
 //
 //  Created by Nilay Pachauri on 11/18/20.
@@ -8,11 +8,37 @@
 import Firebase
 import Foundation
 
-class ModelUtility {
+class FirestoreWrapper {
     
     static let db = Firestore.firestore()
+    static private let counterRef = FirestoreWrapper.db.collection("count").document("counters")
     
-    func 
+    static func getCluesForTriviaGauntlet(numOfClues: Int) -> [Clue] {
+        return []
+    }
+    
+    static private func getCounterData() -> Counter? {
+        FirestoreWrapper.counterRef.getDocument { (document, error) in
+            let result = Result {
+                try document?.data()
+            }
+            switch result {
+            case .success(let counter):
+                if let counter = counter {
+                    // A `Counter` value was successfully initialized from the DocumentSnapshot.
+                    print("Counter: \(counter)")
+                } else {
+                    // A nil value was successfully initialized from the DocumentSnapshot,
+                    // or the DocumentSnapshot was nil.
+                    print("Document does not exist")
+                }
+            case .failure(let error):
+                // A `Counter` value could not be initialized from the DocumentSnapshot.
+                print("Error decoding counter: \(error)")
+            }
+        }
+        return nil
+    }
     
     
 }
