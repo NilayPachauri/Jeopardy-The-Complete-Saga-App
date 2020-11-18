d90ad7e5759554eec6a51b76f91bf9c22c12afc2
nilaypachauri <pachauri@usc.edu>
2020-11-18 13:37:58 -0800

    Added Class Implementation of Counter

--- Jeopardy! The Complete Saga/Jeopardy! The Complete Saga/Model/Counter.swift
+++ Jeopardy! The Complete Saga/Jeopardy! The Complete Saga/Model/Counter.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+class Counter: Codable {
+    
+    // MARK: - Private Class Attributes
+    private var jeopardyCategoriesCount: Int
+    private var doubleJeopardyCategoriesCount: Int
+    private var finalJeopardyCategoriesCount: Int
+    
+    // MARK: - Initializers
+    init(jeopardyCategoriesCount: Int, doubleJeopardyCategoriesCount: Int, finalJeopardyCategoriesCount: Int) {
+        self.jeopardyCategoriesCount = jeopardyCategoriesCount
+        self.doubleJeopardyCategoriesCount = doubleJeopardyCategoriesCount
+        self.finalJeopardyCategoriesCount = finalJeopardyCategoriesCount
+    }
+    
+    // MARK: - Getters
+    func getJeopardyCategoriesCount() -> Int {
+        return self.jeopardyCategoriesCount
+    }
+    
+    func getDoubleJeopardyCategoriesCount() -> Int {
+        return self.doubleJeopardyCategoriesCount
+    }
+    
+    func getFinalJeopardyCategoriesCount() -> Int {
+        return self.finalJeopardyCategoriesCount
+    }
+}
