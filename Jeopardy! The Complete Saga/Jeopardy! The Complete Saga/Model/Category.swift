ea429d0bfc119198d907444452f9bb8bf6273b8a
nilaypachauri <pachauri@usc.edu>
2020-11-19 05:13:57 -0800

    Made Class Category which is Codable

--- Jeopardy! The Complete Saga/Jeopardy! The Complete Saga/Model/Category.swift
+++ Jeopardy! The Complete Saga/Jeopardy! The Complete Saga/Model/Category.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+class Category: Codable {
+    
+    // MARK: - Private Class Attributes
+    private(set) var airDate: String?
+    private(set) var category: String?
+    private(set) var categoryID: Int?
+    private(set) var clues: [String : [String : String]]?
+    private(set) var episode: Int?
+    private(set) var season: String?
+    
+    // MARK: - Coding Key
+    enum CodingKeys: String, CodingKey {
+        case airDate = "air_date"
+        case category
+        case categoryID
+        case clues
+        case episode
+        case season
+    }
+}
