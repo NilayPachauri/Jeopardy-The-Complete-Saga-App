61487c3521b08aea10f9b5d93c3c626848603b1d
nilaypachauri <pachauri@usc.edu>
2020-11-20 00:09:31 -0800

    Added Results Page View Controller and Implementation

---
+++ Jeopardy! The Complete Saga/Jeopardy! The Complete Saga/Controller/ResultsPageViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ResultsPageViewController.swift
+//  Jeopardy! The Complete Saga
+//
+//  Created by Nilay Pachauri on 11/20/20.
+//
+
+import UIKit
+
+class ResultsPageViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    // MARK: - ViewController Lifecycle Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.makeNavigationBarTransparent()
+        self.setResultsLabel()
+    }
+    
+    // MARK: - Functions to Set Up View
+    func makeNavigationBarTransparent() {
+        
+    }
+    
+    func setResultsLabel() {
+        self.resultsLabel.text = String(format: "You got %d out of %d correct!", TriviaGauntletGame.shared.getScore(), TriviaGauntletGame.shared.getNumberOfClues())
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
